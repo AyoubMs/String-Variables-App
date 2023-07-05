d02552c0ffdf71a5ea2f419405f8742f63a2be1f
AyoubMS <chahayb563@gmail.com>
2023-07-05 14:35:22 +0200

    add app

---
+++ StringVariables/Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+string firstName = string.Empty;
+string lastName = string.Empty;
+string filePath = string.Empty;
+
+// expertsExchange
+// expert sex change
+
+firstName = "Tim";
+lastName = "Corey";
+//filePath = "C:\\Temp\\Demo";
+filePath = @"C:\Temp\Demo";
+//firstName = "123";
+
+
+//Console.WriteLine(firstName + " " + lastName);
+
+string testString = $@"The file for {firstName} is at {filePath}";
+
+// String interpolation
+Console.WriteLine($"Hello {firstName} {lastName}");
+Console.WriteLine(filePath);
+Console.WriteLine(testString);
+
